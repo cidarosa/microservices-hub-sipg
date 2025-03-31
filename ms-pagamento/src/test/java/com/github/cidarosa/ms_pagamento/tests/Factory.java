52e26cc1b93aa2e056ad2f71278441bec66d54fe
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-31 11:47:05 -0300

    tests: add Factory, createPagamento Test

---
+++ ms-pagamento/src/test/java/com/github/cidarosa/ms_pagamento/tests/Factory.java
@@ -0,0 +1,17 @@
+package com.github.cidarosa.ms_pagamento.tests;
+
+import com.github.cidarosa.ms_pagamento.entity.Pagamento;
+import com.github.cidarosa.ms_pagamento.entity.Status;
+
+import java.math.BigDecimal;
+
+public class Factory {
+
+    public static Pagamento createPagamento(){
+        Pagamento pagamento = new Pagamento(1L, BigDecimal.valueOf(32.25),
+                "Jon Snow", "2345789632145789",
+                "07/32", "585", Status.CRIADO, 1L, 2L);
+
+        return  pagamento;
+    }
+}
