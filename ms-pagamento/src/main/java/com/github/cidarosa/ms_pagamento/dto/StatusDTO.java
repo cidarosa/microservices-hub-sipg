7b773559892f6fee6a5d4a16b69ab6531c7ce05c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-11 10:40:23 -0300

    add http.pedidoclient, collection postman

--- ms-pagamento/src/main/java/com/github/cidarosa/ms_pagamento/dto/StatusDTO.java
+++ ms-pagamento/src/main/java/com/github/cidarosa/ms_pagamento/dto/StatusDTO.java
@@ -1,8 +1,12 @@
 package com.github.cidarosa.ms_pagamento.dto;
 
 import com.github.cidarosa.ms_pagamento.entity.Status;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class StatusDTO {
 
